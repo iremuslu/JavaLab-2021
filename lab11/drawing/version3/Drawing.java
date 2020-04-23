4fdde9ac30fbc6f865aeb29d962f33f3f549ad16
ozgurkilic10 <ozgur.kilic10@gmail.com>
2020-04-23 10:21:36 +0300

    lab 11

---
+++ lab11/drawing/version3/Drawing.java
@@ -0,0 +1,33 @@
+package drawing.version3;
+
+import java.util.ArrayList;
+
+import shapes.Circle;
+import shapes.Rectangle;
+
+public class Drawing {
+	
+	private ArrayList<Circle> circles = new ArrayList<Circle>();
+	private ArrayList<Rectangle> rectangles = new ArrayList<Rectangle>();
+	
+	public double calculateTotalArea(){
+		double totalArea = 0;
+
+		for (Circle circle : circles){
+			totalArea += circle.area();    
+		}
+		
+		for (Rectangle rect : rectangles){            
+			totalArea += rect.area();     
+		}		
+		return totalArea;
+	}
+	
+	public void addCircle(Circle c) {
+		circles.add(c);
+	}
+	
+	public void addRectangle(Rectangle r) {
+		rectangles.add(r);
+	}
+}
