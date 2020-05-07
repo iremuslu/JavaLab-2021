aac852c9baf2ef96df300de2bc22030f77e8ded2
ozgurkilic10 <ozgur.kilic10@gmail.com>
2020-05-07 11:07:14 +0300

    lab 13

--- lab13/tictactoe/Main.java
+++ lab13/tictactoe/Main.java
@@ -1,32 +1,34 @@
 package tictactoe;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args)  {
-		Scanner reader = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
 
-		Board board = new Board();
-		
-		System.out.println(board);
-		while (!board.isEnded()) {
-			
-			int player = board.getCurrentPlayer();
-			
-			System.out.print("Player "+ player + " enter row number:");
-			int row = reader.nextInt();
-			System.out.print("Player "+ player + " enter column number:");
-			int col = reader.nextInt();
-			
-			board.move(row, col);
-			System.out.println(board);
-		}
-		
-		
-		reader.close();
-	}
+        Board board = new Board();
 
+        System.out.println(board);
+        while (!board.isEnded()) {
+
+            int player = board.getCurrentPlayer();
+
+            System.out.print("Player " + player + " enter row number:");
+            int row = Integer.valueOf(reader.nextLine());
+
+
+            System.out.print("Player " + player + " enter column number:");
+            int col = Integer.valueOf(reader.nextLine());
+
+            board.move(row, col);
+            System.out.println(board);
+        }
+
+
+        reader.close();
+    }
 
 
 }
