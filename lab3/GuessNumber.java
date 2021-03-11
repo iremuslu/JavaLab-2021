2f013bac30bc7351dff5851792f0a07737c6c696
U170709043 <iremuslu@posta.mu.edu.tr>
2021-03-11 15:04:49 +0300

    lab3

--- lab3/GuessNumber.java
+++ lab3/GuessNumber.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,12 +9,33 @@ public class GuessNumber {
 		Scanner reader = new Scanner(System.in); //Creates an object to read user input
 		Random rand = new Random(); //Creates an object from Random class
 		int number =rand.nextInt(100); //generates a number between 0 and 99
-		
-		
+
 		System.out.println("Hi! I'm thinking of a number between 0 and 99.");
 		System.out.print("Can you guess it: ");
-		
-		int guess = reader.nextInt(); //Read the user input
+		int guess = reader.nextInt(); //Read the user input1
+
+		int count=1;
+		while(number!=guess && guess!=-1){
+			System.out.println("Sorry!");
+			if(guess < number){
+				System.out.println("Mine ise greater than your guess.");
+			}
+
+			else{
+				System.out.println("Mine ise less than your guess.");
+			}
+			System.out.print("Type -1 to quit or guess another:");
+			guess= reader.nextInt();
+			count++;
+		}
+
+		if(guess == number){
+			System.out.println("Congratulations ! Congratulations! You won after "+count+" attemps!" );
+		}
+
+		else{
+			System.out.println("Sorry,the number was "+ number);
+		}
 		
 		
 		
