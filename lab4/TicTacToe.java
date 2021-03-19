48c2889a43ebb1f43e2cbd565c3da8e05ce3dba1
U170709043 <iremuslu@posta.mu.edu.tr>
2021-03-19 11:16:30 +0300

    lab4

--- lab4/TicTacToe.java
+++ lab4/TicTacToe.java
@@ -5,44 +5,110 @@ public class TicTacToe {
 
 	public static void main(String[] args) throws IOException {
 		Scanner reader = new Scanner(System.in);
-		char[][] board = { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
+		char[][] board = {{' ', ' ', ' '},
+				          {' ', ' ', ' '},
+				          {' ', ' ', ' '}};
 
 		printBoard(board);
 
-		System.out.print("Player 1 enter row number:");
-		int row = reader.nextInt();
-		System.out.print("Player 1 enter column number:");
-		int col = reader.nextInt();
-		board[row - 1][col - 1] = 'X';
-		printBoard(board);
+		int moveCount = 0;
+		int currentPlayer = 0;
 
-		System.out.print("Player 2 enter row number:");
-		row = reader.nextInt();
-		System.out.print("Player 2 enter column number:");
-		col = reader.nextInt();
-		board[row - 1][col - 1] = 'O';
-		printBoard(board);
+		while (moveCount < 9) {
 
+			System.out.print("Player " + (currentPlayer + 1) + " enter row number:");
+			int row = reader.nextInt();
+			System.out.print("Player " + (currentPlayer + 1) + " enter column number:");
+			int col = reader.nextInt();
+			if (row > 0 && row < 4 && col > 0 && col < 4 && board[row - 1][col - 1] == ' ') {
+				if (currentPlayer == 0) {
+					board[row - 1][col - 1] = 'X';
+				}
+				else {
+					board[row - 1][col - 1] = 'O';
+				}
+				printBoard(board);
+
+				boolean win = checkBoard(board , row-1 , col-1);
+				if(win){
+					System.out.println("Player " + (currentPlayer + 1) + " has won the game");
+					break;
+				}
+				currentPlayer = (currentPlayer + 1) % 2;
+				moveCount++;
+			}
+			else {
+				System.out.println("It's a not valid location");
+			}
+		}
 		reader.close();
 	}
 
-	public static void printBoard(char[][] board) {
-		System.out.println("    1   2   3");
-		System.out.println("   -----------");
-		for (int row = 0; row < 3; ++row) {
-			System.out.print(row + 1 + " ");
-			for (int col = 0; col < 3; ++col) {
-				System.out.print("|");
-				System.out.print(" " + board[row][col] + " ");
-				if (col == 2)
-					System.out.print("|");
+	public static boolean checkBoard(char[][] board, int row, int col) {
 
+		char symbol = board [row][col];
+		boolean win = true;
+
+		for(int i = 0 ; i < 3 ; i++){
+			if(symbol != board[row][i]){
+				win=false;
 			}
-			System.out.println();
-			System.out.println("   -----------");
-
+		}
+		if(win){
+			return true;
 		}
 
+		win= true;
+		for(int j = 0 ; j < 3 ;j++){
+			if(symbol != board[j][col]){
+				win=false;
+			}
+		}
+		if(win){
+			return true;
+		}
+
+		if(row==col){
+			win=true;
+			for(int i=0 ,j=0;i<3; i++,j++){
+				if(symbol != board[i][j]) {
+					win = false;
+				}
+			}
+			if(win){
+				return true;
+			}
+		}
+
+		if(row+col==2){
+			win=true;
+			for(int i=2 ,j=0;j<3; i--,j++){
+				if(symbol != board[i][j]) {
+					win = false;
+				}
+			}
+			if(win){
+				return true;
+			}
+		}
+		return win;
 	}
 
-}
+	public static void printBoard ( char[][] board){
+			System.out.println("    1   2   3");
+			System.out.println("   -----------");
+			for (int row = 0; row < 3; ++row) {
+				System.out.print(row + 1 + " ");
+				for (int col = 0; col < 3; ++col) {
+					System.out.print("|");
+					System.out.print(" " + board[row][col] + " ");
+					if (col == 2)
+						System.out.print("|");
+
+				}
+				System.out.println();
+				System.out.println("   -----------");
+
+			}
+		}
+	}
